aae933bcb4bda15dab251be09579365b28377b47
giangdo888 <giangdo1998@gmail.com>
2023-07-23 15:31:17 +0700

    improve performance by reading chunk by chunk and not the whole file at once

--- readLineQuick/readLineQuick.cpp
+++ readLineQuick/readLineQuick.cpp
@@ -2,36 +2,41 @@
 #include <chrono>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/mman.h>
 #include <cstring>
-//#include <string>
-//#include <algorithm>
+
 using namespace std;
 using namespace chrono;
+const int ALLOWED_MEMORY_SIZE = 10000;
 
 int main()
 {
     int timer = duration_cast<milliseconds> (system_clock::now().time_since_epoch()).count();
-    int file_write = open("test.txt", O_CREAT | O_RDWR, 
-                    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-    posix_fallocate(file_write, 0, 7000000);
-
-    auto first = reinterpret_cast<char*>(
-        mmap(NULL, 7000000, PROT_READ, MAP_SHARED, file_write, 0)
-    );
-
-    // string fileString = first;
-    // char key = '\n';
-    // int count = count_if(fileString.begin(), fileString.end(), [&key] (char fromString) {
-    //     return key == fromString;
-    // });
-
+    int fileDescriptor = open("test.txt", O_SYNC | 0 | O_RDONLY);
+    size_t length = lseek(fileDescriptor, 0, SEEK_END);
     int count = 0;
-    char *occ = strchr(first, '\n');
-    while (occ != NULL) {
-        count++;
-        occ = strchr(occ+1, '\n');
+    int offset = 0;
+    char buffer[ALLOWED_MEMORY_SIZE + 1];
+    int size = ALLOWED_MEMORY_SIZE;
+
+    while (1) {
+        buffer[ALLOWED_MEMORY_SIZE] = {'\0'};
+        pread(fileDescriptor, buffer, size, offset);
+        char *occ = strchr(buffer, '\n');
+        while (occ != NULL) {
+            count++;
+            occ = strchr(occ+1, '\n');
+        }
+
+        offset += ALLOWED_MEMORY_SIZE;
+        if (offset + ALLOWED_MEMORY_SIZE >= length) {
+            size = length - offset;
+            buffer[size + 1] = {'\0'};
+        }
+        if (offset > length) {
+            break;
+        }
     }
+    count++;
     timer = duration_cast<milliseconds> (system_clock::now().time_since_epoch()).count() - timer;
     cout << count <<" were counted in " << timer << endl;
     return 0;
