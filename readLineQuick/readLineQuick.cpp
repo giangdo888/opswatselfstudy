8e7e8822e4aff0f2d229cd01bbdcf0133a2353df
giangdo888 <giangdo1998@gmail.com>
2023-08-10 22:27:18 +0700

    multiple files at once

--- readLineQuick/readLineQuick.cpp
+++ readLineQuick/readLineQuick.cpp
@@ -8,36 +8,55 @@ using namespace std;
 using namespace chrono;
 const int ALLOWED_MEMORY_SIZE = 10000;
 
+void getNextFileName(string &skeleton, int &fileNumberConter);
+
 int main()
 {
     int timer = duration_cast<milliseconds> (system_clock::now().time_since_epoch()).count();
-    int fileDescriptor = open("test.txt", O_SYNC | 0 | O_RDONLY);
-    size_t length = lseek(fileDescriptor, 0, SEEK_END);
-    int count = 0;
-    int offset = 0;
-    char buffer[ALLOWED_MEMORY_SIZE + 1];
-    int size = ALLOWED_MEMORY_SIZE;
+    string skeleton;
+    int fileNumberConter = 0;
 
-    while (1) {
-        buffer[ALLOWED_MEMORY_SIZE] = {'\0'};
-        pread(fileDescriptor, buffer, size, offset);
-        char *occ = strchr(buffer, '\n');
-        while (occ != NULL) {
-            count++;
-            occ = strchr(occ+1, '\n');
-        }
+    getNextFileName(skeleton, fileNumberConter);
+    int fileDescriptor = open(skeleton.c_str(), O_SYNC | 0 | O_RDONLY);
+    while(fileDescriptor != -1) {
+        size_t length = lseek(fileDescriptor, 0, SEEK_END);
+        int count = 0;
+        int offset = 0;
+        char buffer[ALLOWED_MEMORY_SIZE + 1];
+        int size = ALLOWED_MEMORY_SIZE;
 
-        offset += ALLOWED_MEMORY_SIZE;
-        if (offset + ALLOWED_MEMORY_SIZE >= length) {
-            size = length - offset;
-            buffer[size + 1] = {'\0'};
+        while (1) {
+            buffer[ALLOWED_MEMORY_SIZE] = {'\0'};
+            pread(fileDescriptor, buffer, size, offset);
+            char *occ = strchr(buffer, '\n');
+            while (occ != NULL) {
+                count++;
+                occ = strchr(occ+1, '\n');
+            }
+
+            offset += ALLOWED_MEMORY_SIZE;
+            if (offset + ALLOWED_MEMORY_SIZE >= length) {
+                size = length - offset;
+                buffer[size + 1] = {'\0'};
+            }
+            if (offset > length) {
+                break;
+            }
         }
-        if (offset > length) {
-            break;
-        }
+        count++;
+        cout << count <<" lines were counted from file " << skeleton << endl;
+    
+        getNextFileName(skeleton, fileNumberConter);
+        fileDescriptor = open(skeleton.c_str(), O_SYNC | 0 | O_RDONLY);
     }
-    count++;
     timer = duration_cast<milliseconds> (system_clock::now().time_since_epoch()).count() - timer;
-    cout << count <<" were counted in " << timer << endl;
+    cout << fileNumberConter - 1 << " files were counted in " << timer << "s" << endl;
     return 0;
+}
+
+void getNextFileName(string &skeleton, int &fileNumberConter)
+{
+    skeleton = "test_@@@.txt";
+    fileNumberConter++;
+    skeleton.replace(skeleton.find("@@@"), 3, to_string(fileNumberConter));
 }
